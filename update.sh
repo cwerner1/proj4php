8a8533ed158274c3f410be887dcc02d134a804e9
Christian Werner <christian.werner@the-wogo.com>
2012-06-29 21:08:44 +0200

    updated

--- update.sh
+++ update.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-search="Proj4php::$common->adjustLon"
+search='Proj4php::$common->adjustLon'
 replace="Proj4php_common::adjustLon"
 list=$(find . -name \*.php)
 for i in $list; do
-	sed "s/$search/$replace/" $i > _$i
-	mv $i ${i#_}
+	sed -e "s/$search/$replace/" $i
 done
